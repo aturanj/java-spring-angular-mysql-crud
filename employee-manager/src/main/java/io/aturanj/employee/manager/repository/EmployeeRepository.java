d49a4a207f3126cb4665b402195aaf2b7cb7d9de
a.turan <aturan@tuta.io>
2021-05-10 03:30:45 +0100

    updated back-end

--- employee-manager/src/main/java/io/aturanj/employee/manager/repository/EmployeeRepository.java
+++ employee-manager/src/main/java/io/aturanj/employee/manager/repository/EmployeeRepository.java
@@ -3,10 +3,12 @@ package io.aturanj.employee.manager.repository;
 import io.aturanj.employee.manager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    @Transactional
     void deleteEmployeeById(Long employeeId);
 
     Optional<Employee>  findEmployeeById(Long employeeId);
